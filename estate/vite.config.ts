9f6c5ee015f79b7f76b0169963795e171a5f175d
Fortune Obioha <fortuneobioha926@gmail.com>
2025-07-16 10:53:46 +0100

    pages added

--- estate/vite.config.ts
+++ estate/vite.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
           input: {
             main: resolve(__dirname, 'index.html'),
             about: resolve(__dirname, 'about.html'),
+            agent: resolve(__dirname, 'agent.html'),
             contact: resolve(__dirname, 'contact.html'),
+            property: resolve(__dirname, 'property.html'),
+            propertydetails: resolve(__dirname, 'propertydetails.html'),
           },
         },
       },
